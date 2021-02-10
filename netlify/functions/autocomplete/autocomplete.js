93a8be8f2af91b5627ad922c48bba109990f3294
mcolo <max.miracolo@gmail.com>
2021-02-10 17:54:57 -0500

    add lookup function

--- netlify/functions/autocomplete/autocomplete.js
+++ netlify/functions/autocomplete/autocomplete.js
@@ -3,15 +3,77 @@ const movieData = require("./autocomplete_dataset.json", "utf-8");
 
 exports.handler = async (event, context) => {
   const search = event.queryStringParameters.search || "World";
-  if (trie && movieData) {
+  if (!search) {
     return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `You searched for ${search}` }),
+      status: 404,
+      body: JSON.stringify({ message: "No search term" }),
     };
-  } else {
+  }
+
+  try {
+    const ids = suggestions(search);
+
+    if (ids) {
+      const results = getMovieData(ids);
+      return {
+        statusCode: 200,
+        body: JSON.stringify(results),
+      };
+    } else {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "No movie data found" }),
+      };
+    }
+  } catch (err) {
     return {
-      statusCode: 400,
-      body: JSON.stringify({ message: "Didn't Work" }),
+      statusCode: 500,
+      body: JSON.stringify({ message: "Failed to look through dataset" }),
     };
   }
 };
+
+function suggestions(str) {
+  let response = [];
+  str = str.toLowerCase();
+  try {
+    const startNode = str.split("").reduce((node, char) => {
+      return node[char];
+    }, trie);
+    response = getIds(startNode);
+    return response;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+}
+
+function getIds(obj, ids = []) {
+  if (obj["ids"]) {
+    ids.push(...obj["ids"]);
+  }
+
+  for (let prop in obj) {
+    if (prop !== "ids") {
+      getIds(obj[prop], ids);
+    }
+  }
+  return ids;
+}
+
+function getMovieData(ids) {
+  let data = [];
+  ids.forEach((id, index) => {
+    if (movieData[id].r) {
+      data.push({
+        title: movieData[id].t,
+        year: movieData[id].y,
+        rating: movieData[id].r,
+        id,
+      });
+    }
+  });
+  // data.sort( (a,b) => b.rating - a.rating);
+  data.splice(9, data.length - 10);
+  return data;
+}
