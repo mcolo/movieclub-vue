7af1312bf8ed7019c4bcfe8535c9c55339548818
mcolo <max.miracolo@gmail.com>
2021-02-10 17:48:02 -0500

    add json files to function

--- netlify/functions/autocomplete/autocomplete.js
+++ netlify/functions/autocomplete/autocomplete.js
@@ -1,8 +1,17 @@
+const trie = require("./autocomplete_trie.json", "utf8");
+const movieData = require("./autocomplete_dataset.json", "utf-8");
+
 exports.handler = async (event, context) => {
   const search = event.queryStringParameters.search || "World";
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: `You searched for ${search}` }),
-  };
+  if (trie && movieData) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `You searched for ${search}` }),
+    };
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Didn't Work" }),
+    };
+  }
 };
