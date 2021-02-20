adc154708dfe2b823a549e245c6b27a50cdfeb26
mcolo <max.miracolo@gmail.com>
2021-02-19 20:47:17 -0500

    switch api and add styling

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,26 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Hind:wght@400;700&display=swap");
+
 body {
   margin: 0;
   padding: 0;
   width: 100%;
   height: 100%;
+  background-color: #f9fafb;
+}
+
+.logo {
+  font-family: "Hind", sans-serif;
+  font-weight: 700;
+  font-size: 16px;
+  color: #1a202e;
+  text-transform: uppercase;
+  margin: 10px 15px;
+  /* width: 50%;
+  margin: 15px auto;
+  min-width: 720px; */
 }
 
 #app {
--- src/pages/Picks.vue
+++ src/pages/Picks.vue
@@ -1,83 +1,158 @@
 <template>
   <div>
-    <h1 v-if="title">{{ title }}</h1>
-    <p>Movie Data:</p>
-    <div>{{ movieData }}</div>
+    <header>
+      <h1 class="logo">
+        movieclub <span class="title">{{ title }}</span>
+      </h1>
+    </header>
+    <section class="posters">
+      <img
+        v-for="movie in movieData"
+        class="poster"
+        :src="movie.Poster"
+        :alt="movie.Title"
+        :key="movie.imdbID"
+        :class="{ selected: selectedMovie === movie.imdbID }"
+        @click="selectedMovie = movie.imdbID"
+      />
+    </section>
+    <section class="details">
+      <template v-if="selectedMovieDetails">
+        <h3 class="details__title">{{ selectedMovieDetails.Title }}</h3>
+        <p class="details__specs">
+          <span class="spec">{{ selectedMovieDetails.Runtime }}</span>
+          <span class="spec">{{ selectedMovieDetails.Year }}</span>
+          <span class="spec">{{ selectedMovieDetails.Genre }}</span>
+        </p>
+        <p class="details__plot">{{ selectedMovieDetails.Plot }}</p>
+        <p class="details__director">
+          Directed by {{ selectedMovieDetails.Director }}
+        </p>
+        <p class="details__starring">
+          Starring {{ selectedMovieDetails.Actors }}
+        </p>
+      </template>
+    </section>
   </div>
 </template>
 
 <script>
-import { getPicksFromId, getMovieDataFromIds } from "../services/picks-service";
+import { getPicksFromId } from "../services/picks-service";
 export default {
   data() {
     return {
-      picks: [],
+      // picks: [],
       movieData: null,
       title: "",
+      selectedMovie: null,
     };
   },
   methods: {
     async populateData() {
-      const { pickData, error } = await getPicksFromId(this.$route.params.id);
+      const { picks, error } = await getPicksFromId(this.$route.params.id);
       if (error) return;
+      this.movieData = picks.data;
+      this.title = picks.title;
+      this.selectedMovie = this.movieData[0].imdbID;
       /**
        * TODO
        * add error message in UI
        */
-      this.picks = JSON.parse(pickData.picks);
-      this.title = pickData.title;
-      const ids = this.picks.map((pick) => pick.id);
-      const { movieData, error: movieDataError } = await getMovieDataFromIds(
-        ids
-      );
-      if (movieDataError) return;
-      /**
-       * TODO
-       * add error message in UI
-       */
-      this.movieData = movieData;
 
-      // const url =
-      //   "https://fathomless-reaches-08772.herokuapp.com/api/picks/" +
-      //   this.$route.params.id;
-      // const options = {
-      //   method: "GET",
-      //   mode: "cors",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      // };
-      // fetch(url, options)
-      //   .then((res) => res.json())
-      //   .then((res) => {
-      //     this.picks = JSON.parse(res.picks);
-      //     this.title = res.title;
-      //     this.getImdbData();
-      //   })
-      //   .catch((err) => console.log(err));
+      // this.picks = JSON.parse(pickData.picks);
+      // this.title = pickData.title;
+      // const ids = this.picks.map((pick) => pick.id);
+      // const { movieData, error: movieDataError } = await getMovieDataFromIds(
+      //   ids
+      // );
+      // if (movieDataError) return;
+      // this.movieData = movieData;
+      /**
+       * TODO
+       * add error message in UI
+       */
     },
-    // getImdbData() {
-    //   const ids = this.picks.map((pick) => pick.id);
-    //   const url =
-    //     "https://fathomless-reaches-08772.herokuapp.com/api/movieData/";
-    //   const options = {
-    //     method: "POST",
-    //     mode: "cors",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({ ids }),
-    //   };
-    //   fetch(url, options)
-    //     .then((res) => res.json())
-    //     .then((res) => {
-    //       this.movieData = res;
-    //     })
-    //     .catch((err) => console.log(err));
+    // selectMovie(id) {
+    //   this.selectedMovie = this.movieData.find((movie) => movie.imdbID === id);
     // },
   },
   mounted() {
     this.populateData();
   },
+  computed: {
+    selectedMovieDetails() {
+      if (!this.movieData) return null;
+
+      return this.movieData.find(
+        (movie) => movie.imdbID === this.selectedMovie
+      );
+    },
+  },
 };
 </script>
+
+<style scoped>
+.title {
+  color: #f98080;
+  margin-left: 10px;
+}
+
+.posters {
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+  justify-content: center;
+  padding: 35px 50px;
+}
+
+.posters::-webkit-scrollbar {
+  display: none;
+}
+
+.poster {
+  width: 150px;
+  height: 222px;
+  /* aspect-ratio: 1 / 1.48; */
+  border-radius: 8px;
+  box-shadow: 0px 0px 10px rgb(0 0 0 / 20%);
+  margin: 0 0 0 30px;
+  cursor: pointer;
+  transition: 150ms;
+}
+
+.poster:hover {
+  transform: translateY(4px);
+}
+
+.poster.selected {
+  transform: translateY(20px);
+}
+
+.poster:not(.selected):active {
+  transform: translateY(4px) scale(0.95);
+  box-shadow: 0px 0px 7px rgb(0 0 0 / 45%);
+}
+
+.details {
+  /* background-color: #1a202e;
+  color: #fff; */
+  font-family: "Hind", sans-serif;
+  font-weight: 400;
+  width: 50%;
+  margin: 0 auto;
+  min-width: 720px;
+}
+
+.details__title {
+  font-size: 42px;
+  margin: 0;
+}
+
+.details__specs {
+  margin: 0;
+}
+
+.details__specs .spec {
+  margin-right: 15px;
+}
+</style>
